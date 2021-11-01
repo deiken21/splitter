b960c84563cc4fbc01e3285b365d411a0d59d980
deiken21 <ajanth21@live.co.uk>
2021-11-01 14:38:21 +0000

    add colour

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 "use strict"
+function split4(){
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+    addQuarter(this)
+}
+function addQuarter(intoDiv){
+    let q = document.createElement("div")
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+    q.addEventListener("click",split4)
+    q.style.backgroundColor=`rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`
+    intoDiv.appendChild(q)
 
-alert("hello")
+}
+document.body.addEventListener("click",split4)
